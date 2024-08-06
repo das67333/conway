a645f1dc160b7abd7bccc9e258a85a35b518128f
das67333 <spkazimov@edu.hse.ru>
2024-08-07 00:23:26 +0300

    Hashmap research (2)

--- src/bin/bench_hashmap/main.rs
+++ src/bin/bench_hashmap/main.rs
@@ -1,42 +1,45 @@
 mod memory;
 use memory::{Manager, NodeIdx};
-use rand::{Rng, SeedableRng};
+use rand::{seq::SliceRandom, Rng, SeedableRng};
 use rand_chacha::ChaCha8Rng;
 
 fn main() {
-    let n = 1e7 as u32;
+    let n = 50_000_000;
 
-    let mut mem = Manager::new();
+    let mut mem = Manager::with_capacity((n as usize).next_power_of_two());
 
-    let mut rng =  ChaCha8Rng::seed_from_u64(42);
-    let indices = (0..n).map(|_| rng.gen::<u32>()).collect::<Vec<_>>();
+    let mut rng = ChaCha8Rng::seed_from_u64(42);
+    let mut indices = (0..n)
+        .map(|_| [0; 4].map(|_| rng.gen::<u32>()))
+        .collect::<Vec<_>>();
     assert_eq!(mem.storage_size - 1, 0);
 
     let t = std::time::Instant::now();
 
-    for i in indices.iter() {
+    for arr in indices.iter() {
         mem.find_node(
-            NodeIdx::new(i + 0),
-            NodeIdx::new(i + 1),
-            NodeIdx::new(i + 2),
-            NodeIdx::new(i + 3),
+            NodeIdx::new(arr[0]),
+            NodeIdx::new(arr[1]),
+            NodeIdx::new(arr[2]),
+            NodeIdx::new(arr[3]),
         );
     }
 
     println!("{} ns per insert", t.elapsed().as_nanos() / n as u128);
-    assert_eq!(mem.storage_size - 1, 8380430 /* n as usize */);
+    assert_eq!(mem.storage_size - 1, n as usize);
 
+    indices.shuffle(&mut rng);
     let t = std::time::Instant::now();
 
-    for i in indices.iter() {
+    for arr in indices.iter() {
         mem.find_node(
-            NodeIdx::new(i + 0),
-            NodeIdx::new(i + 1),
-            NodeIdx::new(i + 2),
-            NodeIdx::new(i + 3),
+            NodeIdx::new(arr[0]),
+            NodeIdx::new(arr[1]),
+            NodeIdx::new(arr[2]),
+            NodeIdx::new(arr[3]),
         );
     }
 
     println!("{} ns per find", t.elapsed().as_nanos() / n as u128);
-    assert_eq!(mem.storage_size - 1, 8380430 /* n as usize */);
+    assert_eq!(mem.storage_size - 1, n as usize);
 }
