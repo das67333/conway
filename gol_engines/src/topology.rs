af3cac3bfd3a48bbdcd91b9065d600310755eae4
das67333 <spkazimov@edu.hse.ru>
2025-05-14 18:47:18 +0300

    Moved gol_engines to https://github.com/das67333/gol_engines

--- gol_engines/src/topology.rs
+++
@@ -1,8 +0,0 @@
-/// Describes the strategy of updating the field.
-#[derive(Clone, Copy, Debug, PartialEq)]
-pub enum Topology {
-    /// Opposite bounds of the field are stitched together.
-    Torus,
-    /// Field is unbounded and can grow infinitely.
-    Unbounded,
-}
